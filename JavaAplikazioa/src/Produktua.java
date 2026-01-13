062ab9936e699664694f1eb145ab3a6f88c63107
Ares <ikaperez25@lhusurbil.eus>
2026-01-13 10:04:48 +0100

    a

---
+++ JavaAplikazioa/src/Produktua.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Produktua {
+    private String ean13;
+    private String izena;
+    private Kategoria kategoriaNagusia;
+
+    private List<Kategoria> bigarrenMailakoKategoria;
+    private Map<String, String> atributuak;
+
+    public Produktua(String ean13, String izena, Kategoria kategoriaNagusia) {
+        this.ean13 = ean13;
+        this.izena = izena;
+        this.kategoriaNagusia = kategoriaNagusia;
+        this.bigarrenMailakoKategoria = new ArrayList<>();
+        this.atributuak = new HashMap<>();
+
+    }
+}
